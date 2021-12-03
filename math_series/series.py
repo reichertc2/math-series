03176d942766a1106a62c4e81fff8eeb9bae3a26
Chris Reichert <chris.a.reichert@gmail.com>
2021-12-03 09:36:49 -0600

    completed lab 02, with some help from classmates

--- math_series/series.py
+++ math_series/series.py
@@ -1,13 +1,23 @@
 def fibonacci_one(n):
-    new_list = [0, ]
-    for item in range(0, n):
-        new_list.append(1)
-        new_list.append(new_list[item]+new_list[item+1])
+    
+    if n == 0:
+        return 0
+    elif n ==1:
+        return 1
+    
+    return fibonacci_one(n-1) + fibonacci_one(n-2)
+    
+def lucas(n):
+    if n==0:
+        return 2
+    elif n ==1:
+        return 1
+    return lucas(n-1)+lucas(n-2)
 
-        # if n == 1:
-        #     return 1
-        # return fibonacci_one(n-1) + n
-    return new_list
+def sum_series(n,i=0,j=1):
+    if n == 0:
+        return i
+    elif n ==1:
+        return j
+    return sum_series(n-1,i,j)+sum_series(n-2,i,j)
 
-
-fibonacci_one(3)
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,27 +1,61 @@
-from math_series.series import fibonacci_one
+from math_series.series import fibonacci_one, lucas, sum_series
 import pytest
 # actual = function return
 # expected = expecting it to return
 # assert actual is == to expected
 
-
+# @pytest.mark.skip(reason="no yet")
 def test_fibonacci_one_a():
     n = 1
     actual = fibonacci_one(1)
     expected = n
     assert actual == expected
 
-
+# @pytest.mark.skip(reason="no yet")
 def test_fibonacci_one_b():
-    n = 0
-    actual = fibonacci_one(2)
-    expected = [n, 1, 1]
+    actual = fibonacci_one(0)
+    expected = 0
+    assert actual == expected
+
+# @pytest.mark.skip(reason="no yet")
+def test_fibonacci_one_c():
+    actual = fibonacci_one(9)
+    expected = 34
+    assert actual == expected
+
+# @pytest.mark.skip(reason="no yet")
+def test_lucas_a():
+    actual = lucas(1)
+    expected = 1
+    assert actual == expected
+
+# @pytest.mark.skip(reason="no yet")
+def test_lucas_b():
+    actual = lucas(0)
+    expected = 2
     assert actual == expected
 
 
-@pytest.mark.skip(reason="no yet")
-def test_fibonacci_one_c():
-    n = 0
-    actual = fibonacci_one(9)
-    expected = 34
+# @pytest.mark.skip(reason="no yet")
+def test_lucas_c():
+    actual = lucas(5)
+    expected = 11
+    assert actual == expected
+
+# @pytest.mark.skip(reason="no yet")
+def test_sum_series_a():
+    actual = sum_series(0)
+    expected = 0
+    assert actual == expected
+
+# @pytest.mark.skip(reason="no yet")
+def test_sum_series_b():
+    actual = sum_series(1)
+    expected = 1
+    assert actual == expected
+
+# @pytest.mark.skip(reason="no yet")
+def test_sum_series_c():
+    actual = sum_series(5, 5)
+    expected = 20
     assert actual == expected
